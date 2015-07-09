b88ef4869be0db541c40eae8fa0e20b05752ff45
Warshavski <shikari005@gmail.com>
2015-07-09 20:15:28 +0300

    Add matrix creation and list filling

--- graphModel.cpp
+++ graphModel.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <cctype>
 
 using namespace std;
 
@@ -29,6 +30,8 @@ link *initializeList(const int listSize)
 	return list;
 }
 
+/*
+*/
 void printList(link *list, const int listSize)
 {
 	for (int i = 0; i < listSize; ++i)
@@ -39,13 +42,121 @@ void printList(link *list, const int listSize)
 			cout << tempNode->id << " -> ";
 			tempNode = tempNode->next;
 		}
-		cout << endl;
+		cout << "end" << endl;
 	}
 	
 }
 
+void printList(link *list, const int listSize, char path[])
+{
+	/*
+	char buff[1024];
+	ifstream myfile(path);
+	
+	int i = 0;
+	if (myfile.is_open())
+	{
+		while (!myfile.eof())
+		{
+			myfile.getline(buff,1024); 
+			ParseString(buff);
+			arr[i].no = atoi(valuesArr[0]);
+			strcpy(arr[i].fio, valuesArr[1]);
+			strcpy(arr[i].facult, valuesArr[2]);
+			strcpy(arr[i].shifr, valuesArr[3]);
+			++i;
+		}
+		myfile.close();
+	}
+	else
+		cout << "Can't open file";
+	*/
+}
+
+void insertElement(link ch)
+{
+	int elementID = 0;
+	int contactID = 0;
+	
+	bool uspex = true;
+	while(uspex)
+	{
+		cout << "enter id : ";
+		if (!(cin >> elementID))
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			//cout << "Invalid input.  Try again: ";
+			uspex = false;
+		}
+		else
+		{
+			
+			cout << "\tinput contact id :";
+			cin >> contactID;
+			if (ch->id != 0)
+			{
+				ch = (ch->next = new node(elementID, contactID, NULL));
+			}
+			else
+			{
+				ch->id = elementID;
+				ch->contact = contactID;
+			}
+			cout << endl;
+		}
+	}
+}
+
+int **initializeMatrix(const int matrixSize)
+{
+	int **matrix = new int*[matrixSize];
+	for (int i = 0; i < matrixSize; ++i)
+	{
+		matrix[i] = new int[matrixSize];
+		for (int j = 0; j < matrixSize; ++j)
+			matrix[i][j] = 0;
+	}
+	return matrix;
+}
+
+void printMatrix(int **matrix, int matrixSize)
+{
+	for (int i = 0; i < matrixSize; ++i)
+	{
+		for (int j = 0; j < matrixSize; ++j)
+			cout << matrix[i][j] << " ";
+		cout << endl;
+	}
+}
+
+int **createMatrix(link *list, const int listSize, int matrixSize)
+{
+	int **matrix = initializeMatrix(matrixSize);
+	for (int i = 0; i < listSize; ++i)
+	{
+		link tempHead = list[i];
+		
+		while (tempHead->next != NULL)
+		{
+			link tempNode = list[i];
+			while (tempNode->next != NULL)
+			{
+				if (tempHead->id != tempNode->id)
+					++matrix[tempHead->id][tempNode->id];
+				tempNode = tempNode->next;
+				
+			}
+			tempHead = tempHead->next;
+		}
+	}
+	return matrix;
+}
+
 int main()
 {
+	// кол-во элементов, пока х3 как подсчитать. Вводи вручную
+	const int matrixSize = 5;
 	int listSize = 0;
 	
 	cout << "input list size : ";
@@ -56,25 +167,23 @@ int main()
 	for (int i = 0; i < listSize; ++i)
 	{
 		link tempNode = chain[i];
-		int contactID = 0;
-		int elementID = 0;
-		
-		cout << "input element id : ";
-		while (cin >> elementID)
-		{
-			cout << "input contact : ";
-			cin >> contactID;
-			if (tempNode->id == 0)
-			{
-				tempNode->id = contactID;
-				tempNode->contact = contactID;
-			}
-			else
-			{
-				tempNode = (tempNode->next = new node(elementID, contactID, NULL));
-			}
-		}
+		insertElement(tempNode);	
 	}
 	
 	printList(chain, listSize);
+	
+	int **matrix = createMatrix(chain, listSize, matrixSize);
+	
+	printMatrix(matrix, matrixSize);
+	
 }
+
+
+
+
+
+
+
+
+
+
