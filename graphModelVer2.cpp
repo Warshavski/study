9219ba3a58ef879f5fe759cf37c9715bc3acd866
Warshavski <shikari005@gmail.com>
2015-07-11 13:27:07 +0300

    add read/write file

--- graphModelVer2.cpp
+++ graphModelVer2.cpp
@@ -158,12 +158,32 @@ void printChainList(chainLink listHead)
 	}
 }
 
+void printChainList(chainLink listHead, char path[])
+{
+	ofstream outfile;
+	outfile.open(path);
+	
+	chainLink tempHead = listHead;
+	while (tempHead != NULL)
+	{
+		outfile << tempHead->id << "," << tempHead->contactCount << "\n";
+		nodeLink  tempNode = tempHead->elementList;
+		while (tempNode != NULL)
+		{
+			outfile << tempNode->id << "," << tempNode->contactNum << "\n";
+			tempNode = tempNode->next;
+		}
+		outfile << "-\n";
+		tempHead = tempHead->next;
+	}
+}
+
 int main()
 {
-	const int LIST_SIZE = 3;
-	chainLink chainsList = createChainList(LIST_SIZE);
-	printChainList(chainsList);
-	cout << endl;
+	//const int LIST_SIZE = 3;
+	//chainLink chainsList = createChainList(LIST_SIZE);
+	//printChainList(chainsList);
+	//cout << endl;
 	
 	
 	
@@ -174,6 +194,78 @@ int main()
 	
 	//cout << "input list size : ";
 	//cin >> listSize;
+	
+	
+	char path[] = "test.txt";
+	ifstream outfile;
+	outfile.open(path);
+	
+	char buff[50];
+	char end[] = "-";
+	
+	chainLink listHead = new chain(0, 0, NULL, NULL);
+	chainLink tempHead = listHead;
+	
+	nodeLink listNode = NULL;
+	nodeLink tempNode = NULL;
+	
+	bool isChain = true;
+	while(!outfile.eof())
+	{
+		outfile >> buff;
+		//cout << buff << endl;
+		if (strcmp(buff, end) == 0)
+		{
+			tempHead->elementList = listNode;
+			tempHead = (tempHead->next = new chain(0, 0, NULL, NULL));
+			
+			listNode = NULL;
+			tempNode = NULL;
+			
+			isChain = true;
+		}
+		else
+		{
+			char *pch = strtok (buff, ",:"); // во втором параметре указаны разделитель (пробел, запятая, точка, тире)
+			
+			int count = 0;
+			int valBuff[2];
+			while (pch != NULL)                         // пока есть лексемы
+			{
+				valBuff[count++] = (int)(*pch - 48);
+				pch = strtok (NULL, ",:");
+			}
+			
+			if (isChain)
+			{
+				//cout << valBuff[0] << "|" << valBuff[1] << endl;
+				tempHead->id = valBuff[0];
+				tempHead->contactCount = valBuff[1];
+				isChain = false;
+			}
+			else
+			{
+				if (listNode == NULL)
+				{
+					listNode = new node(valBuff[0], valBuff[1], NULL);
+					tempNode = listNode;
+				}
+				else
+				{
+					tempNode = (tempNode->next = new node(valBuff[0], valBuff[1], NULL));
+				}
+			}
+		}	
+	}
+	outfile.close();
+	
+	printChainList(listHead);
+	printChainList(listHead, "ntest.txt");
+	
+	return 0;
+	
+	
+	
 	
 	
 	
